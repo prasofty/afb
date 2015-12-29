d6deb655f841aaf35089030ada2c7d079663854e
prasanna.boga <prasofty@gmail.com>
2015-12-29 15:09:12 +0530

    2nd changes

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,4 +1,32 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require angular
-//= require_tree .
+//= require angular-form-builder/angular-form-builder
+//= require angular-form-builder/angular-form-builder-components
+//= require angular-validator/angular-validator
+//= require angular-validator/angular-validator-rules
+
+'use strict';
+
+var afb = angular.module('afb', ['builder', 'builder.components', 'validator.rules']);
+
+afb.run([
+    '$builder', function($builder) {
+
+    }
+]);
+
+afb.controller('newController', [
+    '$scope', '$builder', '$validator', function($scope, $builder, $validator) {
+
+        $scope.form = $builder.forms['default'];
+
+        return $scope.submit = function() {
+            return $validator.validate($scope, 'default').success(function() {
+                return console.log('success');
+            }).error(function() {
+                return console.log('error');
+            });
+        };
+    }
+]);
